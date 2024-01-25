571bf897dcb568e0ff68dec1e315017627bf6b79
KwCCCC <andy020817@naver.com>
2024-01-25 17:22:25 +0900

    initial commit

--- acmicpc/start.sh
+++ acmicpc/start.sh
@@ -10,3 +10,28 @@ int main() {
 }
 " > $1/$1.cpp
 > $1/input.txt
+
+echo "# [난이도] [문제 이름]
+
+[문제 링크]() 
+
+### 성능 요약
+
+### 분류
+
+<details><summary>문제 분류</summary> 
+
+[내용]
+
+</details>
+
+### 문제 요약 및 해결 방안 스케치
+
+### Solution
+
+<details><summary>Solution</summary> 
+
+[Source Code]
+
+</details>" > $1/README.md
+
